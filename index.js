9059173592b3f3a897cb8c475152a2fe9ddb26f9
Samira Gabriely <samieditor2020@gmail.com>
2021-12-11 16:26:46 -0300

    Adicionando cabeçalho

--- index.js
+++ index.js
@@ -3,31 +3,47 @@ let opiniao = document.querySelector("#opiniao");
 let botaoSim = document.querySelector("#sim");
 let botaoNao = document.querySelector("#nao");
 
-botaoSim.addEventListener("click", function() {
-   opiniao.textContent = `Agradecemos sua visita. Volte sempre :)`;
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    });
+}
+const titulo = document.querySelector('h1');
+typeWriter(titulo);
+typeWriter(document.querySelector('h3'));
+
+botaoSim.addEventListener("click", function () {
+    opiniao.textContent = `Agradecemos sua visita. Volte sempre :)`;
 });
 
-botaoNao.addEventListener("click", function() {
+botaoNao.addEventListener("click", function () {
     let corpo = document.querySelector("body");
     let texto = document.createElement("p");
     let i = 0;
     opiniao.style.display = "none";
+
+    var elemento = document.querySelector(".cabecalho");
+    elemento.parentNode.removeChild(elemento);
 
     while (cards[i] != undefined) {
         cards[i].style.display = "none";
         i++;
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         texto.textContent = `Então`;
         corpo.appendChild(texto);
 
     }, 5000);
-    setTimeout(function() {
+    setTimeout(function () {
         texto.textContent = `Nesse caso`;
         corpo.appendChild(texto);
     }, 10000);
-    setTimeout(function() {
+    setTimeout(function () {
         texto.textContent = `Fique sem ver a página`;
         corpo.appendChild(texto);
     }, 15000);
